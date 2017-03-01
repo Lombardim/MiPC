3e09f03938a88584ebbd4e2159d9317792c88971
lombardim <lombardim@su1-28.uninorteacad.uninorte.edu.co>
2017-03-01 16:21:15 -0500

    Edits

---
+++ src/tupc/Escritorio.java
@@ -0,0 +1,11 @@
+package tupc;
+
+public class Escritorio extends Equipo{
+    
+    public Escritorio(String serial, String marca, String modelo, Procesador procesador, DiscoDuro discoduro, float memoriaram) {
+        
+        super(serial, marca, modelo, procesador, discoduro, memoriaram);
+        
+    }
+       
+}
